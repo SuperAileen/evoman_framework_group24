ebdae81e85d00dd19835bf0b9929954fcc9b4334
Ailuro <ailuroluna.l@gmail.com>
2024-10-12 22:47:39 +0200

    add training for generalist

---
+++ line_plot_10_runs_generalist.py
@@ -0,0 +1,89 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from training_generalist import GeneralistOptimizer  # Updated import
+import pandas as pd
+
+def run_optimizer(mode, enemy_set):
+    # Set headless mode for faster experiments
+    headless = True
+    if headless:
+        os.environ["SDL_VIDEODRIVER"] = "dummy"
+
+    experiment_name = 'optimization_generalist'
+
+    # Parameters from the generalist file
+    n_hidden_neurons = 10
+    n_population = 100
+    n_generations = 30
+    mutation_rate = 0.2
+    sigma = 0.1
+
+    optimizer = GeneralistOptimizer(experiment_name, enemy_set, n_hidden_neurons, n_population, n_generations,
+                                    mutation_rate, sigma, mode=mode)
+    stats_path = optimizer.execute()
+
+    if os.path.exists(stats_path):
+        return stats_path
+    else:
+        raise Exception(f"Could not find stats.txt path for {mode}")
+
+def aggregate_stats(mode, enemy_set, runs):
+    all_data = []
+    for _ in range(runs):
+        stats_path = run_optimizer(mode, enemy_set)
+        data = np.genfromtxt(stats_path, skip_header=1)
+        all_data.append(data)
+
+    all_data = np.array(all_data)
+    mean_data = np.mean(all_data, axis=0)
+    std_data = np.std(all_data, axis=0)
+
+    aggregated_data = np.column_stack(
+        (mean_data[:, 0], mean_data[:, 1], mean_data[:, 2], std_data[:, 1], std_data[:, 2]))
+    return aggregated_data
+
+def plot_aggregated_stats(aggregated_data_1, aggregated_data_2, enemy_set, num_runs):
+    generations_1 = aggregated_data_1[:, 0]
+    avg_fitness_1 = aggregated_data_1[:, 1]
+    max_fitness_1 = aggregated_data_1[:, 2]
+    std_dev_1 = aggregated_data_1[:, 3]
+
+    generations_2 = aggregated_data_2[:, 0]
+    avg_fitness_2 = aggregated_data_2[:, 1]
+    max_fitness_2 = aggregated_data_2[:, 2]
+    std_dev_2 = aggregated_data_2[:, 3]
+
+    plt.figure(figsize=(10, 6))
+
+    # Plot GA
+    plt.plot(generations_1, avg_fitness_1, label='GA - Average Fitness', color='red', linestyle='--')
+    plt.fill_between(generations_1, avg_fitness_1 - std_dev_1, avg_fitness_1 + std_dev_1, color='red', alpha=0.2)
+    plt.plot(generations_1, max_fitness_1, label='GA - Max Fitness', color='red')
+    plt.fill_between(generations_1, max_fitness_1 - std_dev_1, max_fitness_1 + std_dev_1, color='red', alpha=0.2)
+
+    # Plot ES
+    plt.plot(generations_2, avg_fitness_2, label='ES - Average Fitness', color='blue', linestyle='--')
+    plt.fill_between(generations_2, avg_fitness_2 - std_dev_2, avg_fitness_2 + std_dev_2, color='blue', alpha=0.2)
+    plt.plot(generations_2, max_fitness_2, label='ES - Max Fitness', color='blue')
+    plt.fill_between(generations_2, max_fitness_2 - std_dev_2, max_fitness_2 + std_dev_2, color='blue', alpha=0.2)
+
+    plt.title(f'Aggregated Fitness over Generations for GA and ES - Enemies {enemy_set} ({num_runs} runs)')
+    plt.xlabel('Generation')
+    plt.ylabel('Fitness')
+    plt.legend()
+    plt.grid(False)
+    plt.xlim(min(generations_1.min(), generations_2.min()) - 1, max(generations_1.max(), generations_2.max()) + 1)
+    plt.savefig(f'Aggregated_Fitness_GA_ES_Enemies_{enemy_set}_{num_runs}_runs.png')
+    plt.show()
+
+if __name__ == "__main__":
+    num_runs = 10
+
+    enemy_sets = [[1, 3, 4, 6], [2, 5, 7, 8]]
+
+    for enemy_set in enemy_sets:
+        aggregated_data_1 = aggregate_stats("GA", enemy_set, num_runs)
+        aggregated_data_2 = aggregate_stats("ES", enemy_set, num_runs)
+
+        plot_aggregated_stats(aggregated_data_1, aggregated_data_2, enemy_set, num_runs)
---
+++ training_generalist.py
@@ -0,0 +1,238 @@
+import sys
+import os
+import time
+import numpy as np
+from math import fabs, sqrt
+import glob
+import sys
+
+from evoman.environment import Environment
+from demo_controller import player_controller
+
+from deap import base, creator, tools, algorithms
+
+import datetime
+
+class GeneralistOptimizer:
+    def __init__(self, base_experiment_name, enemy_set, n_hidden_neurons=10, n_population=100, n_generations=30,
+                 mutation_rate=0.2, sigma=0.1, mode="GA"):
+
+        enemy_string = '_'.join(map(str, enemy_set))
+        parent_directory = f'experiments_train_generalist_{enemy_string}'
+
+        if not os.path.exists(parent_directory):
+            os.makedirs(parent_directory)
+
+        current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        self.experiment_name = os.path.join(parent_directory, f"{base_experiment_name}_generalist_{mode}_{current_time}")
+
+        if not os.path.exists(self.experiment_name):
+            os.makedirs(self.experiment_name)
+
+        self.enemy_set = enemy_set
+        self.n_hidden_neurons = n_hidden_neurons
+        self.n_population = n_population
+        self.n_generations = n_generations
+        self.mutation_rate = mutation_rate
+        self.sigma = sigma
+        self.mode = mode
+
+        self.env = self.setup_environment()
+        self.n_vars = self.calculate_num_weights()
+
+        self.toolbox = base.Toolbox()
+
+        if self.mode == "GA":
+            self.setup_ga()
+        elif self.mode == "ES":
+            self.setup_es()
+        else:
+            print("Hey! You can only choose GA or ES!")
+            return
+
+    def setup_environment(self):
+        env = Environment(experiment_name=self.experiment_name,
+                          enemies=self.enemy_set,
+                          multiplemode="yes",
+                          playermode="ai",
+                          player_controller=player_controller(self.n_hidden_neurons),
+                          enemymode="static",
+                          level=2,
+                          speed="fastest",
+                          visuals=False)
+        env.state_to_log()
+        return env
+
+    def calculate_num_weights(self):
+        # Assuming the environment has a consistent sensor configuration
+        return (self.env.get_num_sensors() + 1) * self.n_hidden_neurons + (self.n_hidden_neurons + 1) * 5
+
+    def setup_ga(self):
+        dom_u = 1
+        dom_l = -1
+
+        creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+        creator.create("Individual", np.ndarray, fitness=creator.FitnessMax)
+
+        self.toolbox.register("attr_float", np.random.uniform, dom_l, dom_u)
+        self.toolbox.register("individual", tools.initRepeat, creator.Individual, self.toolbox.attr_float,
+                              n=self.n_vars)
+        self.toolbox.register("population", tools.initRepeat, list, self.toolbox.individual)
+
+        self.toolbox.register("evaluate", self.evaluate)
+        self.toolbox.register("mate", tools.cxBlend, alpha=0.5)
+        self.toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=1, indpb=self.mutation_rate)
+        self.toolbox.register("select", tools.selTournament, tournsize=3)
+
+    def setup_es(self):
+        dom_u = 1
+        dom_l = -1
+
+        creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+        creator.create("Individual", np.ndarray, fitness=creator.FitnessMax)
+
+        self.toolbox.register("attr_float", np.random.uniform, dom_l, dom_u)
+        self.toolbox.register("individual", tools.initRepeat, creator.Individual, self.toolbox.attr_float, n=self.n_vars)
+        self.toolbox.register("population", tools.initRepeat, list, self.toolbox.individual)
+
+        self.toolbox.register("evaluate", self.evaluate)
+        self.toolbox.register("mutate", self.mutate_es)
+        self.toolbox.register("select", tools.selBest)
+
+    def evaluate(self, individual):
+        fitness = self.env.play(pcont=np.array(individual))[0]
+        return fitness,
+
+    def mutate_es(self, individual):
+        noise = np.random.normal(0, self.sigma, len(individual))
+        individual += noise
+        return individual,
+
+    def run(self):
+        if self.mode == "GA":
+            return self.run_ga()
+        elif self.mode == "ES":
+            return self.run_es()
+
+        print("Invalid mode encountered during run.")
+
+    def run_ga(self):
+        population = self.toolbox.population(n=self.n_population)
+
+        fitnesses = list(map(self.toolbox.evaluate, population))
+        for ind, fit in zip(population, fitnesses):
+            ind.fitness.values = fit
+
+        stats = tools.Statistics(lambda ind: ind.fitness.values)
+        stats.register("avg", np.mean)
+        stats.register("std", np.std)
+        stats.register("min", np.min)
+        stats.register("max", np.max)
+
+        hof = tools.HallOfFame(1, similar=np.array_equal)
+
+        population, logbook = algorithms.eaSimple(population, self.toolbox, cxpb=0.5, mutpb=self.mutation_rate,
+                                                  ngen=self.n_generations, stats=stats, halloffame=hof, verbose=True)
+
+        best = hof[0]
+        np.savetxt(f'{self.experiment_name}/best.txt', best)
+
+        return population, logbook, hof
+
+    def run_es(self):
+        population = self.toolbox.population(n=self.n_population)
+
+        fitnesses = list(map(self.toolbox.evaluate, population))
+        for ind, fit in zip(population, fitnesses):
+            ind.fitness.values = fit
+
+        logbook = tools.Logbook()
+        logbook.header = ['gen', 'avg', 'max', 'std']
+
+        hof = tools.HallOfFame(1, similar=np.array_equal)
+
+        stats = tools.Statistics(lambda ind: ind.fitness.values)
+        stats.register("avg", np.mean)
+        stats.register("std", np.std)
+        stats.register("min", np.min)
+        stats.register("max", np.max)
+
+        record = stats.compile(population)
+        logbook.record(gen=0, **record)
+
+        for gen in range(1, self.n_generations + 1):
+            parents = list(map(self.toolbox.clone, population))
+            offspring = parents
+            for mutant in offspring:
+                self.toolbox.mutate(mutant)
+                mutant.fitness.values = self.evaluate(mutant)
+
+            offspring = offspring + parents
+
+            population[:] = self.toolbox.select(offspring, len(population))
+
+            hof.update(population)
+            record = stats.compile(population)
+            logbook.record(gen=gen, **record)
+            print(f"Gen {gen}: {record}")
+
+        best = hof[0]
+        np.savetxt(f'{self.experiment_name}/best.txt', best)
+
+        return population, logbook, hof
+
+    def save_results(self):
+        file = open(f'{self.experiment_name}/neuroended', 'w')
+        file.close()
+        self.env.state_to_log()
+
+    def execute(self):
+        ini = time.time()
+
+        if self.mode == "GA":
+            print('Starting Genetic Algorithm Optimization...')
+        elif self.mode == "ES":
+            print('Starting Evolution Strategies Optimization...')
+        population, logbook, hof = self.run()
+
+        gen = logbook.select("gen")
+        avg = logbook.select("avg")
+        max_ = logbook.select("max")
+        std = logbook.select("std")
+
+        stats_data = np.column_stack((gen, avg, max_, std))
+        stats_file_path = f'{self.experiment_name}/stats.txt'
+        np.savetxt(stats_file_path, stats_data, header='gen avg max std', comments='', fmt='%f')
+
+        fim = time.time()
+        execution_time_minutes = round((fim - ini) / 60)
+        execution_time_seconds = round((fim - ini))
+
+        print(f'\nExecution time: {execution_time_minutes} minutes')
+        print(f'Execution time: {execution_time_seconds} seconds')
+
+        self.save_results()
+        full_path = os.path.abspath(stats_file_path)
+        print(full_path)
+        return full_path
+
+if __name__ == "__main__":
+    headless = True
+    if headless:
+        os.environ["SDL_VIDEODRIVER"] = "dummy"
+
+    experiment_name = 'optimization_generalist'
+    n_hidden_neurons = 10
+    n_population = 100
+    n_generations = 30
+    mutation_rate = 0.2
+    sigma = 0.1
+
+    enemy_sets = [[1, 3, 4, 6], [2, 5, 7, 8]]
+    modes = ["GA", "ES"]
+
+    for enemy_set in enemy_sets:
+        for mode in modes:
+            generalist_optimizer = GeneralistOptimizer(experiment_name, enemy_set, n_hidden_neurons, n_population, n_generations,
+                                                       mutation_rate, sigma, mode=mode)
+            generalist_optimizer.execute()

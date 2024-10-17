5f7e96336654f1c0fd9ba8d21730c588fef15386
Ailuro <ailuroluna.l@gmail.com>
2024-10-17 16:36:12 +0200

    改了图片大小

--- line_plot_10_runs_generalist.py
+++ line_plot_10_runs_generalist.py
@@ -19,10 +19,10 @@ def run_optimizer(mode, enemy_set):
     n_generations = 30
     mutation_rate = 0.2
     sigma = 0.1
-    k = 10
+    k = 5
 
     optimizer = GeneralistOptimizer(experiment_name, enemy_set, n_hidden_neurons, n_population, n_generations,
-                                    mutation_rate, sigma, mode=mode, k = 10)
+                                    mutation_rate, sigma, mode=mode, k = k)
     stats_path = optimizer.execute()
 
     if os.path.exists(stats_path):
@@ -60,24 +60,27 @@ def plot_aggregated_stats(aggregated_data_1, aggregated_data_2, enemy_set, num_r
 
     plt.figure(figsize=(10, 6))
 
-    # Plot GA
     plt.plot(generations_1, avg_fitness_1, label='Diversity-oriented GA - Average Fitness', color='red', linestyle='--')
     plt.fill_between(generations_1, avg_fitness_1 - std_dev_1, avg_fitness_1 + std_dev_1, color='red', alpha=0.2)
     plt.plot(generations_1, max_fitness_1, label='Diversity-oriented GA - Max Fitness', color='red')
     plt.fill_between(generations_1, max_fitness_1 - std_dev_1, max_fitness_1 + std_dev_1, color='red', alpha=0.2)
 
-    # Plot ES
     plt.plot(generations_2, avg_fitness_2, label='Elitism-oriented GA - Average Fitness', color='blue', linestyle='--')
     plt.fill_between(generations_2, avg_fitness_2 - std_dev_2, avg_fitness_2 + std_dev_2, color='blue', alpha=0.2)
     plt.plot(generations_2, max_fitness_2, label='Elitism-oriented GA - Max Fitness', color='blue')
     plt.fill_between(generations_2, max_fitness_2 - std_dev_2, max_fitness_2 + std_dev_2, color='blue', alpha=0.2)
 
-    plt.title(f'Aggregated Fitness over Generations for GA and ES - Enemies {enemy_set} ({num_runs} runs)')
-    plt.xlabel('Generation')
-    plt.ylabel('Fitness')
-    plt.legend()
+    plt.title(f'Fitness over Generations - Enemies {enemy_set} ({num_runs} runs)', fontsize=16)
+    plt.xlabel('Generation', fontsize=14)
+    plt.ylabel('Fitness', fontsize=14)
+
+    plt.ylim(0, 100)
+
+    plt.legend(prop={'size': 12})
+
     plt.grid(False)
     plt.xlim(min(generations_1.min(), generations_2.min()) - 1, max(generations_1.max(), generations_2.max()) + 1)
+
     plt.savefig(f'Aggregated_Fitness_GA_ES_Enemies_{enemy_set}_{num_runs}_runs.png')
     plt.show()
 
